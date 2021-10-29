c690beb3cdddb6ffb284d099a5d4ebabff52a17b
Lander <lander-portero@hotmail.com>
2021-10-29 13:55:07 +0200

    Implementacion compra
    Crea un cliente, una lista de informes y hace la compra y genera los informes. Falta mejorar.

--- src/main/java/com/maquinaVending/TW004_Vending/MainVending.java
+++ src/main/java/com/maquinaVending/TW004_Vending/MainVending.java
@@ -2,6 +2,8 @@ package com.maquinaVending.TW004_Vending;
 
 import java.util.ArrayList;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class MainVending {
 
 	public static void main(String[] args) {
@@ -9,8 +11,10 @@ public class MainVending {
 		int cantUsu=2;
 		double vueltas=0;
 		double dineroUsu=10;
-		System.out.println("Heyyyyy");
-		ArrayList<Venta> informeVentas= new ArrayList<Venta>();
+		
+		Cliente cliente = new Cliente("Juan", 2, 10);
+		
+		ArrayList<Venta> informeVentas= new ArrayList<Venta>();		
 		
 		ArrayList<Refresco> listaRefrescos= new ArrayList<Refresco>();
 		Refresco cocaCola = new Refresco(1, "Coca-Cola", 2.00, 8);
@@ -23,41 +27,61 @@ public class MainVending {
 		
 		Maquina expendedora = new Maquina(1, 500, true, listaRefrescos);
 		
+		int condicion = 1;
+		
 		while (!salir) {
-			textoPantallaInicio(cocaCola, agua, te);
+			textoPantallaInicio(cocaCola, agua, te);					
 			
-			salir = funcionalidadMaquinaExpendedora(salir, cantUsu, vueltas, dineroUsu, cocaCola);
+			switch (condicion) {
+			case 1:
+				hacerCompra(cantUsu, dineroUsu, cocaCola, informeVentas);	
+				condicion = 2;
+				break;
+			case 2:
+				hacerCompra(cantUsu, dineroUsu, agua, informeVentas);
+				condicion = 3;
+				break;
+			case 3:
+				hacerCompra(cantUsu, dineroUsu, te, informeVentas);
+				condicion = 4;				
+				break;
+			case 4:
+				if(informeVentas.size()>0) {
+					for (int i = 0; i < informeVentas.size(); i++) {
+						System.out.println("Cambios " + informeVentas.get(i).getCambios());
+					}
+				}else {
+					System.out.println("Actualmente no hay informes");
+				}
+				salir=true;
+				break;
+			default:
+				salir=true;
+				System.out.println("Escoge una opcion valida.");
+			}
 			
-			salir=true;
+			System.out.println(salir);
 		}
 
 	}
 
-	private static boolean funcionalidadMaquinaExpendedora(boolean salir, int cantUsu, double vueltas, double dineroUsu,
-			Refresco cocaCola) {
-		int condicion = 3;
-		switch (condicion) {
-		case 1:
-			System.out.println("Has escogido Coca cola");
-			System.out.println("Cantidad? " + cantUsu);
-			if(cocaCola.ComprobarActualizarCantidad(cantUsu)) {
-				System.out.println("Debes ingresar: " + (cocaCola.getPrecio()*cantUsu));
-				
-				System.out.println("Has ingresado: " + dineroUsu);
-				
-				System.out.println("Sus vueltas: " + vueltas);
-			}else {
-				System.out.println("No hay cantidad suficiente de este producto");
-			}			
+
+	private static void hacerCompra(int cantUsu, double dineroUsu, Refresco refresco, ArrayList<Venta> listaVentas) {
+		double vueltas;
+		System.out.println("Has escogido " + refresco.getNombre());
+		System.out.println("Cantidad? " + cantUsu);
+		if(refresco.ComprobarActualizarCantidad(cantUsu)) {
+			System.out.println("Debes ingresar: " + (refresco.getPrecio()*cantUsu));
 			
-			break;
-		case 2:
-			break;
-		case 3:
-			salir=true;
-			break;
+			System.out.println("Has ingresado: " + dineroUsu);
+			vueltas = dineroUsu - refresco.getPrecio();
+			System.out.println("Sus vueltas: " + vueltas);
+			
+			Venta venta = new Venta(1, cantUsu, (refresco.getPrecio()*cantUsu), vueltas, refresco);
+			listaVentas.add(venta);
+		}else {
+			System.out.println("No hay cantidad suficiente de este producto");
 		}
-		return salir;
 	}
 
 	private static void textoPantallaInicio(Refresco cocaCola, Refresco agua, Refresco te) {
@@ -65,7 +89,7 @@ public class MainVending {
 		System.out.println("Cod. \t Cant. \t Refresco \t Precio");
 		System.out.println("\n " + cocaCola.getId() + "\t " + cocaCola.getCantidad() + "\t   " + cocaCola.getNombre() + "\t" + cocaCola.getPrecio());
 		System.out.println("\n " + agua.getId() +"\t " + agua.getCantidad() + "\t   " + agua.getNombre() + "\t \t" + agua.getPrecio());
-		System.out.println("\n " + te.getId() +"\t " + te.getCantidad() + "\t   " + te.getNombre() + "\t \t" + te.getPrecio());
+		System.out.println("\n " + te.getId() +"\t " + te.getCantidad() + "\t   " + te.getNombre() + "\t \t" + te.getPrecio() + "\n 4 GENERAR LISTA INFORMES");
 	}
 
 
